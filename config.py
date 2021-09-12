26ae738540aa850718a9fe0ac4ecacdbe6afcba0
Alan da Silva Alves <linuxfama@gmail.com>
2021-09-12 11:26:31 -0300

    feat: working migrate fail

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 # DEBUG = True
 
 USERNAME = 'root'
-PASSAWORD = 'maria@123'
+PASSWORD = 'maria@123'
 SERVER = 'localhost'
 DB = 'flask_fundamentos'
 
-SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSAWORD}@{SERVER}/{DB}'
+SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
+
 SQLALCHEMY_TRACK_MODIFICATIONS = True
